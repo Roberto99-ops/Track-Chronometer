a9755853dd2c05854ca064d752ef19c588af8127
Roberto99-ops <roberto.molgora@mail.polimi.it>
2022-11-22 11:49:35 +0100

    added start/stop button

--- lib/create_view_temps.dart
+++ lib/create_view_temps.dart
@@ -2,22 +2,80 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 
-class CreateViewTemps extends StatelessWidget {
-
+class CreateViewTemps extends StatefulWidget {
 
   const CreateViewTemps({Key? key}) : super(key: key);
 
+  @override
+  _View createState() => _View();
+}
 
+  class _View extends State<CreateViewTemps>{
+  late bool ready;
+
+  @override
+  void initState(){
+    super.initState();
+    ready=true;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //child: Center(
-       //child: //qui ci va il tempo,
-      //),
+    return Column(
+      children: [
+        Container(
+          //qui ci va uil tempo
+          height: MediaQuery.of(context).size.height-250,
+        ),
+      if (ready==true)...[
+        ElevatedButton(
+          onPressed: () {
+            //qui facciamo partire il tutto
+            setState(() { ready=false;});
+          },
+          style: const ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(Colors.green),
+            fixedSize: MaterialStatePropertyAll(Size.fromWidth(200)),
+          ),
+          child: Row(
+            children: const [
+              Icon(Icons.play_arrow, color: Colors.white, size: 60,),
+              SizedBox(
+                width: 5,
+                height: 80,
+              ),
+              Text("Start", style: TextStyle(color: Colors.white),
+                textScaleFactor: 2.5,)
+            ],
+          ),
+        ),
+      ]else...[
+        ElevatedButton(
+          onPressed: () {
+            //qui facciamo partire il tutto
+            setState(() { ready=true;});
+          },
+          style: const ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(Colors.red),
+            fixedSize: MaterialStatePropertyAll(Size.fromWidth(200)),
+          ),
+          child: Row(
+            children: const [
+              Icon(Icons.play_arrow, color: Colors.white, size: 60,),
+              SizedBox(
+                width: 5,
+                height: 80,
+              ),
+              Text("Stop", style: TextStyle(color: Colors.white),
+                textScaleFactor: 2.5,)
+            ],
+          ),
+        ),
+    ],
+    ],
     );
   }
-}
+
 
 
   //this function refactors the text
@@ -26,6 +84,8 @@ class CreateViewTemps extends StatelessWidget {
     int end = string.lastIndexOf("b")-4;
     return string.substring(start, end);
   }
+
+}
 
 
 
---
+++ lib/view_of_savelocalfile.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:app_cronometro/manage_files.dart';
+
+import 'main.dart';
+
+class ViewSaveFile extends StatefulWidget{
+  final String title;
+  final File temps;
+  const ViewSaveFile({Key? key, required this.title, required this.temps}) : super(key: key);
+
+
+  @override
+  _ViewSaveFile createState() => _ViewSaveFile();
+}
+
+class _ViewSaveFile extends State<ViewSaveFile>{
+  bool isReadOnly = true;
+  final titleController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      body: Center(
+        child: Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+                Radius.circular(15.0)
+            ),
+            color: Colors.black,
+          ),
+          height: 200,
+          width: 300,
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                width: 300,
+                height: 10,
+              ),
+              const Text("Insert the name of the file"),
+              SizedBox(
+                width: 280,
+                child: TextField(
+                  controller: titleController,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  readOnly: isReadOnly,
+                  onTap: (){setState(() {isReadOnly=false;});},
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 300,
+                height: 60,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 110,
+                    height: 55,
+                    child: ElevatedButton(
+                        onPressed: () {setState(() {isReadOnly=true;});},
+                        style: const ButtonStyle(
+                          // minimumSize: MaterialStateProperty.,
+                        ),
+                        child: const Text("delete")
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 60,
+                  ),
+                  SizedBox(
+                    width: 110,
+                    height: 55,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        saveFile(titleController.text, widget.title, widget.temps);
+                        await Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => TabApp(), //qui mi sa che è sbagliato nel senso che penso di dover fare la "back"
+                          ),
+                        );
+                      },
+                      child: const Text("save"),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
