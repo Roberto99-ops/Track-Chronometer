6a63f9ce9ba0f7e11c957b72b911dab301d3f1bb
Roberto99-ops <79856152+Roberto99-ops@users.noreply.github.com>
2023-03-15 09:38:42 +0100

    ultrasound implemented instead of IR

--- arduino/Slave/Slave.ino
+++ arduino/Slave/Slave.ino
@@ -1,7 +1,9 @@
 #include <SoftwareSerial.h>
-
+#include <NewPing.h>
 SoftwareSerial HC12(10,11); // HC-12 TX Pin, HC-12 RX Pin
-#define IReceiver 9
+#define TRIG 9
+#define ECO 8
+NewPing sonar(TRIG, ECO, 500); //500 is the max distance in cm
 bool start = false;;
 byte incomingByte;
 String HC = "";
@@ -10,15 +12,14 @@ String HC = "";
 void HCFun();
 
 void setup() {
-  Serial.begin(9600);
-  HC12.begin(9600); 
-  pinMode(IReceiver, INPUT);
+  Serial.begin(1200);
+  HC12.begin(1200); 
   Serial.println("Slave");
 }
 
 
 void loop() {
-  
+  delay(50);
   //it listens on the HC port for incoming messages
   while (HC12.available()) {  // If HC-12 has data
     incomingByte = HC12.read();
@@ -29,8 +30,9 @@ void loop() {
   }
 
   if(start){
-    int photoCell = digitalRead(IReceiver);
-    if (photoCell == HIGH)
+    int ultrasound = sonar.ping_cm();
+    Serial.println(ultrasound);
+    if (ultrasound > 10 && ultrasound < 100) //to prevent errors
     {
       HC12.print("C"); //stops the time
       start = false;
@@ -41,9 +43,12 @@ void loop() {
 
 //this function describes what arduino must do for each different HC input
 void HCFun(){
+  Serial.println(HC);
   if(HC == "A")
-          start=true;
+          {start=true;
+          Serial.println("start");}
   if(HC == "B")
-          start=false;
+          {start=false;
+          Serial.println("stop");}
   HC="";
 }
